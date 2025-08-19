142555f39315bdb04b78addebdde4b93b2347c0b
therapeuti <lorraine.dubled@gmail.com>
2025-08-20 00:19:58 +0900

    feat(#5) register chat blueprint

---
+++ app/routes/chat/chat_route.py
@@ -0,0 +1,9 @@
+from flask import Blueprint, request, jsonify
+
+chat_bp = Blueprint('conversation', __name__)
+
+@chat_bp.route('/chat', methods=['GET'])
+def get_conversation():
+    return jsonify({"message": "Hello from conversation blueprint!"})
+
+# 필요에 따라 추가 라우트
