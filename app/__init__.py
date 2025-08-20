f7b207788fbfbed8aca5bf2502e9bafa110bfcc7
Xuswnss <cooconut0226@gmail.com>
2025-08-20 14:57:29 +0900

    feat(#6) blueprint register

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
-
+from flask import Blueprint
+from app.routes.dailycare import dailycare_bp #dailycare 
 def create_app():
     app = Flask(__name__)
 
@@ -8,6 +9,6 @@ def create_app():
         return render_template('base.html')
 
     # 블루프린트 등록 위치
-    # app.register_blueprint(some_blueprint)
+    app.register_blueprint(dailycare_bp, url_prefix='/dailycare') #dailycare
 
     return app
