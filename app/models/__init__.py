80015d06e75f960e6da894834f7a857d45250729
therapeuti <lorraine.dubled@gmail.com>
2025-08-22 22:44:02 +0900

    db model 수정. Base Model에서 id 삭제

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -8,7 +8,7 @@ def init_db(app):
     # 모든 모델 import (테이블 생성을 위함)
     from app.models.base import BaseModel
     from app.models.user import User
-    from app.models.petPersona import PetPersona
+    from app.models.pet_persona import PetPersona
 
     with app.app_context():
         db.create_all()
@@ -16,7 +16,7 @@ def init_db(app):
 # 모델들을 import하여 다른 모듈에서 사용할 수 있도록 함
 from app.models.base import BaseModel
 from app.models.user import User
-from app.models.petPersona import PetPersona
+from app.models.pet_persona import PetPersona
 
 __all__ = ['db', 'init_db', 'BaseModel', 'User', 'PetPersona']
 
